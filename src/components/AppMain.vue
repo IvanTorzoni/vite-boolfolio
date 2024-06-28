1219a9a3c3cbbef9f87dcb876aba838a70487dc1
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-28 14:09:38 +0200

    feat: test api with console log

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,17 @@
 <script>
-
+import axios from "axios";
+export default {
+    data () {
+        return {
+            projects: []
+        }
+    },
+    created() {
+        axios.get("http://127.0.0.1:8000/api/projects").then((resp) => {
+            console.log(resp)
+        });
+    },
+};
 </script>
 
 <template>
