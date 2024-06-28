27c4b83e19e3b2d26aa3b9e57b09753a0c06987a
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-28 14:06:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import AppMain from "./components/AppMain.vue";
+export default {
+  components: {
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+    <h1 class="text-center">Ciao</h1>
+</template>
+
+<style lang="scss">
+
+</style>
