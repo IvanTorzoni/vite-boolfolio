261c50b1bb50ee60867cb44ad3168673deffaf7f
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-07-02 14:15:08 +0200

    feat: creation page of error 404

---
+++ src/pages/ErrorPage404.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="container text-center mt-3">
+        <h1>ERROR 404, PAGE NOT FOUND</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
